bebb2c069c26aeb1ad7f8543e7dbdf13a0cf7537
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-04-03 19:14:12 -0400

    set up the models in models.py

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
+class Ingredient(models.Model):
+    ingredient_name = models.CharField(max_length=200)
+    ingredient_inv_qty = models.FloatField(validators=[MinValueValidator(0)])
+    ingredient_unit = models.CharField(max_length=200)
+    ingredient_unit_price = models.FloatField(
+        validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.ingredient_name
+
+
+class MenuItem(models.Model):
+    menu_item_name = models.CharField(max_length=200)
+    menu_item_price = models.FloatField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.menu_item_name
+
+
+class RecipeRequirement(models.Model):
+    recipe_qty = models.FloatField(validators=[MinValueValidator(0)])
+    ingredient_name = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    menu_item_name = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+
+
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=200)
+    customer_email = models.EmailField()
+
+    def __str__(self):
+        return self.customer_name
+
+
+class Orders(models.Model):
+    order_timestamp = models.DateTimeField(auto_now=True)
+    customer_name = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order_total = models.FloatField(validators=[MinValueValidator(0)])
+    menu_item_name = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
